605696ade5b7568c2ee2af3f6d9b36cd183fb3c6
Alexandr Ryazanov <ppryaz@yandex.ru>
2021-12-07 19:22:52 +0300

    answer with programm

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,24 @@
 // Напишите ответ здесь:271
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec;
+    int counter = 0;
+    for (int i = 0; i < 1000; ++i)
+    {
+        vec.push_back(i);
+        if (vec[i]/100 == 3 || vec[i]%100/10 == 3 || vec[i]%10 == 3)
+        {counter++;
+        }
+        
+    }
+    cout << counter << endl;
+
+    return 0;
+}
